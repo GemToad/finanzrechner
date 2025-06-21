679ac1462afc6ee73f95af949eaae820ba0f8989
GemToad <D.Woew@yahoo.de>
2025-06-21 08:50:25 +0200

    programmed basic logic

--- finanzrechner/main.py
+++ finanzrechner/main.py
@@ -1,6 +1,28 @@
 from rechner import Rechner
 
 rechner = Rechner()
-rechner.add_einnahme(100.00)
-rechner.add_ausgabe(50.00)
+
+print("Willkommen beim Finanzrechner")
+while True:
+    print("Gib eine Zahl ein:")
+    print("1. Einnahmen hinzufügen ")
+    print("2. Ausgaben hinzufügen ")
+    print("3. Kontostand einsehen ")
+    userInput = int(input())
+
+    while userInput == 1:
+        einnahme = float(input("Einnahme eingeben: "))
+        rechner.add_einnahme(einnahme)
+        if einnahme == 0:
+            print()
+            break
+    while userInput == 2:
+        ausgabe = float(input("Ausgabe eingeben: "))
+        rechner.add_ausgabe(ausgabe)
+        if ausgabe == 0:
+            print()
+            break
+    if userInput == 3:
+        print(f"Dein aktueller Kontostand beträgt: {rechner.berechne_kontostand()} €\n")
+
 
